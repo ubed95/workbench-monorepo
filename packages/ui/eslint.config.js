068b7d65eb04809c97f01503996af1e3cf9c7aba
Ubed Shaikh <ubed.shaikh@kiwiinsurance.com>
2025-11-21 15:33:54 +0530

    fix: fixed app wide errors

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,3 +1,4 @@
+import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -5,19 +6,24 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+      },
     },
   },
 ])
